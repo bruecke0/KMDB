c65659c18ac7f768ac95f2b261d70853847e8201
Rasmus <rasmus.sild@gmail.com>
2024-11-20 12:26:49 +0200

    actorcontroller

--- src/main/java/com/example/filmsociety/controllers/ActorController.java
+++ src/main/java/com/example/filmsociety/controllers/ActorController.java
@@ -1,8 +1,13 @@
 package com.example.filmsociety.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,14 +24,28 @@ public class ActorController {
     private ActorService actorService;
 
     @PostMapping(path = "/api/actors")
-    public Actor createActor(@RequestBody Actor actor) {
-        return actorService.createActor(actor);
+    public ResponseEntity<Actor> createActor(@RequestBody Actor actor) {
+        return ResponseEntity.ok(actorService.createActor(actor));
     }
 
     @GetMapping(path = "/api/actors")
-    public List<Actor> findAllActors(@RequestParam String param) {
-        return actorService.findAllActors();
+    public ResponseEntity<List<Actor>> findAllActors(@RequestParam String name) {
+        return ResponseEntity.ok(actorService.findAllActors());
     }
-    
-    
+
+    @GetMapping(path = "/api/actors/{id}")
+    public ResponseEntity<Optional<Actor>> findActorById(@PathVariable Long id) {
+        return ResponseEntity.ok(actorService.findActorById(id));
+    }
+
+    @PatchMapping(path = "/api/actors/{id}")
+    public ResponseEntity<Actor> updateActor(@PathVariable Long id, @RequestBody Actor updateActor) {
+        return ResponseEntity.ok(actorService.updateActor(id, updateActor));
+    }
+
+    @DeleteMapping(path = "/api/actors/{id}")
+    public ResponseEntity<Void> deleteActor(@PathVariable Long id) {
+        actorService.deleteActor(id);
+        return ResponseEntity.noContent().build();
+    }
 }
